d9a71902c2da34a83e648c7f17267bdee329e164
gukkame <guka245@gmail.com>
2025-01-05 13:55:04 +0200

    Finished Vue + Laravel template

--- laravel-php-template/resources/js/App.vue
+++ laravel-php-template/resources/js/App.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex flex-col min-h-screen m-12">
         <header class="bg-gray-100 p-5">
-            <h1 class="text-2xl font-bold">Welcome to the Registration System</h1>
+            <h1 class="text-2xl font-bold">Welcome to the Vue + Laravel template</h1>
         </header>
         <main class="p-5">
             <router-view></router-view>
         </main>
         <footer class="bg-gray-100 p-5 text-center">
-            <p>&copy; 2023 Registration System</p>
+            <p>&copy; 2024 Made by Gukka</p>
         </footer>
     </div>
 </template>
